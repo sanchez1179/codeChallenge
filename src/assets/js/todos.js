47b99265672381f27b0804233c326ec0412032e6
sanchez1179 <icsanchez1179@gmail.com>
2018-01-02 05:08:35 -0800

    more clean up

--- src/assets/js/todos.js
+++ src/assets/js/todos.js
@@ -1,4 +1,23 @@
+$(document).ready(function(){
 
+    $.ajax(
+        {
+            url: 'http://localhost:8001/getTaskList',
+            type: 'GET',
+            success: function (data) {
+
+                var title = JSON.parse(data);
+
+                var number = title.length;
+
+                for (var i = 0; i < number; i++) {
+
+                    $("#theList").append("<li> <span class='delete'><i class='fa fa-trash' aria-hidden='true'></i></span>"+ title[i]['name'] + "</li>");
+
+                }
+            }
+        })
+})
 
 $("ul").on("click", "li", function(){
     $(this).toggleClass("completed");
